75016007ec413fcc38ad7212f2cbad80185aa7b7
Aleksandr Kamardin <sashka-stalker93@mail.ru>
2023-04-01 19:50:11 +0300

    Homework n.4

--- projects/loft-photo-lite-3/mainPage.js
+++ projects/loft-photo-lite-3/mainPage.js
@@ -1,3 +1,4 @@
+import { doc } from 'prettier';
 import model from './model';
 
 export default {
@@ -6,7 +7,31 @@ export default {
     this.setFriendAndPhoto(friend, id, url);
   },
 
-  setFriendAndPhoto(friend, id, url) {},
+  setFriendAndPhoto(friend, id, url) {
 
-  handleEvents() {},
+    const compPhoto = document.querySelector('.component-photo');
+    const compHeaderPhoto = document.querySelector('.component-header-photo');
+    const compHeaderName = document.querySelector('.component-header-name');
+
+    compHeaderPhoto.style.backgroundImage = `url(${friend.photo_50}')`;
+    compHeaderName.innerText = `${friend.first_name ?? ''} ${friend.last_name ?? ''}`;
+    compPhoto.style.backgroundImage = `url(${url})`;
+  },
+
+  handleEvents() {
+    let startPoint;
+
+    document.querySelector('.component-photo').addEventListener('touchstart', e => {
+      e.preventDefault();
+      startPoint = {y: e.changedTouches[0].pageY};
+    });
+
+    document.querySelector('.component-photo').addEventListener('touchstart', async e => {
+      const direct = e.changedTouches[0].pageY - startPoint.y;
+      if (direct < 0) {
+        await this.getNextPhoto();
+      }
+      
+    });
+  },
 };
--- projects/loft-photo-lite-3/model.js
+++ projects/loft-photo-lite-3/model.js
@@ -1,24 +1,101 @@
 export default {
-  getRandomElement(array) {},
 
-  async getNextPhoto() {},
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: 7565225
+      });
+      
+      VK.Auth.login(data => {
+          if (data.session) {
+              resolve();
+          } else {
+              reject(new Error('Не удалось авторизоваться'));
+          }
+      }, 2);
+    });
+  },
 
-  login() {},
+  callAPI(method, params) {
+    params.v = '5.131';
+  
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if (data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response);
+            }
+        });
+    })
+  },
 
-  init() {},
+  getRandomElement(array) {
+    if(!Array.isArray(array) || array.length === 0) {
+      throw new Error ('empty array');
+    }
+    const ix = parseInt(Math.random() * (array.length - 1));
+    return array[ix];
+  },
 
-  photoCache: {},
-  getFriendPhotos(id) {
-    const photos = this.photoCache[id];
-
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
     if (photos) {
       return photos;
     }
 
-    // const photos = вместо этого комментария вставьте код для получения фотографии друга из ВК
-
+    photos = await this.getFriendPhotos(id);
     this.photoCache[id] = photos;
-
     return photos;
   },
+
+  findPhotoSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) =>{
+        if (current.width > biggest.width) {
+          return current;
+        }
+
+        return biggest;
+      },
+      photo.sizes[0]);
+    }
+    return size;
+  },
+
+  async getNextPhoto(array, ) {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
+    return {
+      friend,
+      id: photo.id,
+      url: photo.url
+    };
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_100', 'photo_50'],
+    };
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(user) {
+    const params = {
+      owner_id: user,
+    };
+    return this.callApi('photos.getAll', params);
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
 };
+
+
+
